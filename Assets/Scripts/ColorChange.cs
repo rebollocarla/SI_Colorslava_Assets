21bfcc0a35f73f4d9f1a9b7dda9e9ce5b2b04a23
Álvaro Jiménez <alvaro.jimenez06@estudiant.upf.edu>
2023-05-25 09:34:25 +0200

    build2 commit

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -8,33 +8,34 @@ public class ColorChange : MonoBehaviour
     public float redProbability = 0.8f; // Probabilidad de que un square sea pintado de rojo (20%)
     public Color defaultColor = Color.green; // Color por defecto de los squares
     public Color hitColor = Color.black; // Color cuando un square es impactado por una pelota
-    private bool isTouchingSquare = false; // Indica si la pelota está en contacto con un square
+    //private bool isTouchingSquare = false; // Indica si la pelota está en contacto con un square
 
-    //private void Start()
-    //{
+    private void Start()
+    {
         // Pintar el square de verde
-    //    GetComponent<Renderer>().material.color = defaultColor;
+        GetComponent<Renderer>().material.color = defaultColor;
 
         // Verificar si el square debe ser pintado de rojo
-    //    if (Random.value <= redProbability)
-    //    {
-    //        GetComponent<Renderer>().material.color = Color.red;
-    //    }
-    //}
+        if (Random.value <= redProbability)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         // Pintar el square de cyan
-        isTouchingSquare = true;
+        //isTouchingSquare = true;
         GetComponent<Renderer>().material.color = hitColor;
     }
 
 
     private void OnCollisionExit(Collision collision)
     {
-        isTouchingSquare = false;
+        //isTouchingSquare = false;
         GetComponent<Renderer>().material.color = defaultColor;
     }
+
 }
 
 
