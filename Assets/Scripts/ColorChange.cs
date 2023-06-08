15e157609274dcdedc039e92edd04d796a5eb20f
Álvaro Jiménez <alvaro.jimenez06@estudiant.upf.edu>
2023-06-08 10:01:54 +0200

    changes

--- Assets/Scripts/ColorChange.cs
+++ Assets/Scripts/ColorChange.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 
 public class ColorChange : MonoBehaviour
 {
-    public CounterValue counterGreen;
+    public CounterValue counterGreen1;
+    public CounterValue counterGreen2;
+    public TimerLives counterLives1;
+    public TimerLives counterLives2;
+    public GameObject player1;
+    public GameObject player2;
+
     public float redProbability = 0.8f; // Probabilidad de que un square sea pintado de rojo (20%)
     public Color defaultColor = Color.green; // Color por defecto de los squares
     public Color hitColor = Color.black; // Color cuando un square es impactado por una pelota
@@ -14,7 +20,8 @@ public class ColorChange : MonoBehaviour
     private void Start()
     {
 
-        counterGreen = GameObject.Find("CounterValue").GetComponent<CounterValue>();
+        counterGreen1 = GameObject.Find("CounterValue").GetComponent<CounterValue>();
+        counterGreen2 = GameObject.Find("CounterValue").GetComponent<CounterValue>();
         // Pintar el square de verde
         GetComponent<Renderer>().material.color = defaultColor;
 
@@ -31,8 +38,26 @@ public class ColorChange : MonoBehaviour
         //isTouchingSquare = true;
         if (GetComponent<Renderer>().material.color == Color.green)
         {
-            Debug.Log("m palabra");
-            counterGreen.AddScore(1);
+            if (collision.gameObject == player1)
+            {
+                counterGreen1.AddScore(1);
+            }
+            else if (collision.gameObject == player2)
+            {
+                counterGreen2.AddScore(1);
+            }
+        }
+
+        if (GetComponent<Renderer>().material.color == Color.red)
+        {
+            if (collision.gameObject == player1)
+            {
+                counterLives1.SubstractScorep1(1);
+            }
+            else if (collision.gameObject == player2)
+            {
+                counterLives2.SubstractScorep2(1);
+            }
 
         }
         GetComponent<Renderer>().material.color = hitColor;
